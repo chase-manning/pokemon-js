310925e8bd8a18f7849cc560074b51e1d4f3c9dc
Chase Manning <chase@manning.dev>
2023-09-04 23:33:55 +0300

    :lipstick: styled app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  background: var(--bg);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const App = () => {
-  return <div>meow</div>;
+  return <StyledApp>meow</StyledApp>;
 };
 
 export default App;

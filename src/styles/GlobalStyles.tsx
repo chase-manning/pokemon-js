9ef6237f7e06a971089e1d1b74e344ea85d0296d
Chase Manning <chase@manning.dev>
2023-09-04 23:33:40 +0300

    :sparkles: add global styles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        --bg: black;
+        --main: white;
+        --sub: grey;
+        --primary: blue;
+    }
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-size: 10px;
+        color: var(--main);
+    }
+
+    button {
+        background: none;
+        border: none;
+        outline: none;
+    }
+    
+    input {
+        border: none;
+        outline: none;
+        background: none;
+
+        // Remove arrows from number input
+        &::-webkit-outer-spin-button,
+        &::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+    }
+
+    a {
+        text-decoration: none;
+    }
+`;
+
+const GlobalStyles = (): JSX.Element => {
+  return <GlobalStyle />;
+};
+
+export default GlobalStyles;

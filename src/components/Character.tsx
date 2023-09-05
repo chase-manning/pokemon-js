5d1c965261322dc56e8f1001fe37a1cf5190b9ee
Chase Manning <chase@manning.dev>
2023-09-05 13:06:01 +0300

    :sparkles: create character componen

---
+++ src/components/Character.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+import frontStill from "../assets/character/front-still.png";
+
+const StyledCharacter = styled.img`
+  position: absolute;
+  top: calc((-16vw / 2.34) / 5);
+  left: 0;
+  width: calc(16vw / 2.34);
+
+  // TODO
+  image-rendering: optimizeSpeed;
+  image-rendering: -moz-crisp-edges;
+  image-rendering: -o-crisp-edges;
+  image-rendering: -webkit-optimize-contrast;
+  image-rendering: pixelated;
+  image-rendering: optimize-contrast;
+  -ms-interpolation-mode: nearest-neighbor;
+`;
+
+const Character = () => {
+  const blockPixelWidth = 16; // TODO
+
+  return <StyledCharacter src={frontStill} alt="Character" />;
+};
+
+export default Character;

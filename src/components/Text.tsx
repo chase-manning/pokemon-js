24ae9c8475efa5f6240f748a4848f6510a08abec
Chase Manning <chase@manning.dev>
2023-09-05 16:47:12 +0300

    :rocket: add completion indicator

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,8 +1,26 @@
 import { useSelector } from "react-redux";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { selectText } from "../state/gameSlice";
+import { useEffect, useState } from "react";
 
-const StyledText = styled.div`
+interface TextProps {
+  done: boolean;
+}
+
+// Flashing animation
+const flashing = keyframes`
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const StyledText = styled.div<TextProps>`
   position: absolute !important;
   left: 0;
   bottom: 0;
@@ -15,18 +33,51 @@ const StyledText = styled.div`
     color: black;
     font-size: 30px;
   }
+
+  ::after {
+    content: "";
+    position: absolute;
+    bottom: ${(props) => (props.done ? "25px" : "-100px")};
+    right: 20px;
+    width: 3px;
+    height: 3px;
+    font-size: 3px;
+    color: #181010;
+    box-shadow: 1em 0em 0 #181010, 2em 0em 0 #181010, 1em 1em 0 #181010,
+      2em 1em 0 #181010, 3em 1em 0 #181010, 1em 2em 0 #181010, 2em 2em 0 #181010,
+      3em 2em 0 #181010, 4em 2em 0 #181010, 1em 3em 0 #181010, 2em 3em 0 #181010,
+      3em 3em 0 #181010, 4em 3em 0 #181010, 5em 3em 0 #181010, 1em 4em 0 #181010,
+      2em 4em 0 #181010, 3em 4em 0 #181010, 4em 4em 0 #181010, 1em 5em 0 #181010,
+      2em 5em 0 #181010, 3em 5em 0 #181010, 1em 6em 0 #181010, 2em 6em 0 #181010;
+    transform: rotate(90deg);
+    animation: ${flashing} 1s infinite;
+  }
 `;
 
 const Text = () => {
   const text = useSelector(selectText);
+  const [liveIndex, setLiveIndex] = useState(0);
+
+  useEffect(() => {
+    setLiveIndex(0);
+    if (text) {
+      const interval = setInterval(() => {
+        setLiveIndex((prev) => prev + 1);
+      }, 30);
+
+      return () => clearInterval(interval);
+    }
+  }, [text]);
 
   if (!text) return null;
+
+  const liveText = text.substring(0, liveIndex);
 
   return (
     <>
       <link rel="stylesheet" href="./styles/css-pokemon-gameboy.css" />
-      <StyledText className="framed">
-        <h1>{text}</h1>
+      <StyledText className="framed" done={liveIndex > text.length}>
+        <h1>{liveText}</h1>
       </StyledText>
     </>
   );

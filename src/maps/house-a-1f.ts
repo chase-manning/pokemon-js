da625e0cece1e32ddb7d7d225531562a055f0794
Chase Manning <chase@manning.dev>
2023-09-06 22:44:27 +0300

    :sparkles: create house a 1f

---
+++ src/maps/house-a-1f.ts
@@ -0,0 +1,55 @@
+import houseA1FImage from "../assets/map/house-a-1f.png";
+import { MapType } from "./map-types";
+
+const houseA1f: MapType = {
+  name: "House A 1F",
+  image: houseA1FImage,
+  height: 8,
+  width: 8,
+  start: {
+    x: 3,
+    y: 7,
+  },
+  walls: {
+    0: {
+      0: true,
+      1: true,
+      2: true,
+      3: true,
+      4: true,
+      5: true,
+      6: true,
+      7: true,
+    },
+    1: {
+      0: true,
+      1: true,
+      3: true,
+      7: true,
+    },
+    4: {
+      2: true,
+      3: true,
+      4: true,
+      5: true,
+    },
+    5: {
+      2: true,
+      3: true,
+      4: true,
+      5: true,
+    },
+  },
+  text: {
+    1: {
+      3: [
+        "Lorem ipsum dolor sit amet, consectetur",
+        "adipiscing elit, sed do eiusmod tempor",
+        "incididunt ut labore et dolore magna aliqua.",
+      ],
+    },
+  },
+  maps: {},
+};
+
+export default houseA1f;

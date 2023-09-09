1ef61b245a620c0f697afdca07cdb578f232ab29
Chase Manning <chase@manning.dev>
2023-09-09 23:03:32 +0300

    :sparkles: add keyboard handling for start and stop moving

--- src/components/KeyboardHandler.tsx
+++ src/components/KeyboardHandler.tsx
@@ -10,15 +10,19 @@ const KeyboardHandler = () => {
       switch (e.key) {
         case "ArrowUp":
           emitter.emit(Event.Up);
+          emitter.emit(Event.StartUp);
           break;
         case "ArrowDown":
           emitter.emit(Event.Down);
+          emitter.emit(Event.StartDown);
           break;
         case "ArrowLeft":
           emitter.emit(Event.Left);
+          emitter.emit(Event.StartLeft);
           break;
         case "ArrowRight":
           emitter.emit(Event.Right);
+          emitter.emit(Event.StartRight);
           break;
         case "Enter":
           emitter.emit(Event.A);
@@ -32,10 +36,29 @@ const KeyboardHandler = () => {
       }
     };
 
+    const handleKeyUp = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+          emitter.emit(Event.StopUp);
+          break;
+        case "ArrowDown":
+          emitter.emit(Event.StopDown);
+          break;
+        case "ArrowLeft":
+          emitter.emit(Event.StopLeft);
+          break;
+        case "ArrowRight":
+          emitter.emit(Event.StopRight);
+          break;
+      }
+    };
+
     window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
     };
   }, [dispatch]);
 

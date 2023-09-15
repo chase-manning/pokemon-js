a0c67c21939bd1e09849a20292001e5161c73636
Chase Manning <chase@manning.dev>
2023-09-15 18:42:35 +0100

    :sparkles: add pokemon data

---
+++ src/app/pokemon-data.ts
@@ -0,0 +1,317 @@
+import pokemon1 from "../assets/pokemon/front/1.png";
+import pokemon2 from "../assets/pokemon/front/2.png";
+import pokemon3 from "../assets/pokemon/front/3.png";
+import pokemon4 from "../assets/pokemon/front/4.png";
+import pokemon5 from "../assets/pokemon/front/5.png";
+import pokemon6 from "../assets/pokemon/front/6.png";
+import pokemon7 from "../assets/pokemon/front/7.png";
+import pokemon8 from "../assets/pokemon/front/8.png";
+import pokemon9 from "../assets/pokemon/front/9.png";
+import pokemon10 from "../assets/pokemon/front/10.png";
+import pokemon11 from "../assets/pokemon/front/11.png";
+import pokemon12 from "../assets/pokemon/front/12.png";
+import pokemon13 from "../assets/pokemon/front/13.png";
+import pokemon14 from "../assets/pokemon/front/14.png";
+import pokemon15 from "../assets/pokemon/front/15.png";
+import pokemon16 from "../assets/pokemon/front/16.png";
+import pokemon17 from "../assets/pokemon/front/17.png";
+import pokemon18 from "../assets/pokemon/front/18.png";
+import pokemon19 from "../assets/pokemon/front/19.png";
+import pokemon20 from "../assets/pokemon/front/20.png";
+import pokemon21 from "../assets/pokemon/front/21.png";
+import pokemon22 from "../assets/pokemon/front/22.png";
+import pokemon23 from "../assets/pokemon/front/23.png";
+import pokemon24 from "../assets/pokemon/front/24.png";
+import pokemon25 from "../assets/pokemon/front/25.png";
+import pokemon26 from "../assets/pokemon/front/26.png";
+import pokemon27 from "../assets/pokemon/front/27.png";
+import pokemon28 from "../assets/pokemon/front/28.png";
+import pokemon29 from "../assets/pokemon/front/29.png";
+import pokemon30 from "../assets/pokemon/front/30.png";
+import pokemon31 from "../assets/pokemon/front/31.png";
+import pokemon32 from "../assets/pokemon/front/32.png";
+import pokemon33 from "../assets/pokemon/front/33.png";
+import pokemon34 from "../assets/pokemon/front/34.png";
+import pokemon35 from "../assets/pokemon/front/35.png";
+import pokemon36 from "../assets/pokemon/front/36.png";
+import pokemon37 from "../assets/pokemon/front/37.png";
+import pokemon38 from "../assets/pokemon/front/38.png";
+import pokemon39 from "../assets/pokemon/front/39.png";
+import pokemon40 from "../assets/pokemon/front/40.png";
+import pokemon41 from "../assets/pokemon/front/41.png";
+import pokemon42 from "../assets/pokemon/front/42.png";
+import pokemon43 from "../assets/pokemon/front/43.png";
+import pokemon44 from "../assets/pokemon/front/44.png";
+import pokemon45 from "../assets/pokemon/front/45.png";
+import pokemon46 from "../assets/pokemon/front/46.png";
+import pokemon47 from "../assets/pokemon/front/47.png";
+import pokemon48 from "../assets/pokemon/front/48.png";
+import pokemon49 from "../assets/pokemon/front/49.png";
+import pokemon50 from "../assets/pokemon/front/50.png";
+import pokemon51 from "../assets/pokemon/front/51.png";
+import pokemon52 from "../assets/pokemon/front/52.png";
+import pokemon53 from "../assets/pokemon/front/53.png";
+import pokemon54 from "../assets/pokemon/front/54.png";
+import pokemon55 from "../assets/pokemon/front/55.png";
+import pokemon56 from "../assets/pokemon/front/56.png";
+import pokemon57 from "../assets/pokemon/front/57.png";
+import pokemon58 from "../assets/pokemon/front/58.png";
+import pokemon59 from "../assets/pokemon/front/59.png";
+import pokemon60 from "../assets/pokemon/front/60.png";
+import pokemon61 from "../assets/pokemon/front/61.png";
+import pokemon62 from "../assets/pokemon/front/62.png";
+import pokemon63 from "../assets/pokemon/front/63.png";
+import pokemon64 from "../assets/pokemon/front/64.png";
+import pokemon65 from "../assets/pokemon/front/65.png";
+import pokemon66 from "../assets/pokemon/front/66.png";
+import pokemon67 from "../assets/pokemon/front/67.png";
+import pokemon68 from "../assets/pokemon/front/68.png";
+import pokemon69 from "../assets/pokemon/front/69.png";
+import pokemon70 from "../assets/pokemon/front/70.png";
+import pokemon71 from "../assets/pokemon/front/71.png";
+import pokemon72 from "../assets/pokemon/front/72.png";
+import pokemon73 from "../assets/pokemon/front/73.png";
+import pokemon74 from "../assets/pokemon/front/74.png";
+import pokemon75 from "../assets/pokemon/front/75.png";
+import pokemon76 from "../assets/pokemon/front/76.png";
+import pokemon77 from "../assets/pokemon/front/77.png";
+import pokemon78 from "../assets/pokemon/front/78.png";
+import pokemon79 from "../assets/pokemon/front/79.png";
+import pokemon80 from "../assets/pokemon/front/80.png";
+import pokemon81 from "../assets/pokemon/front/81.png";
+import pokemon82 from "../assets/pokemon/front/82.png";
+import pokemon83 from "../assets/pokemon/front/83.png";
+import pokemon84 from "../assets/pokemon/front/84.png";
+import pokemon85 from "../assets/pokemon/front/85.png";
+import pokemon86 from "../assets/pokemon/front/86.png";
+import pokemon87 from "../assets/pokemon/front/87.png";
+import pokemon88 from "../assets/pokemon/front/88.png";
+import pokemon89 from "../assets/pokemon/front/89.png";
+import pokemon90 from "../assets/pokemon/front/90.png";
+import pokemon91 from "../assets/pokemon/front/91.png";
+import pokemon92 from "../assets/pokemon/front/92.png";
+import pokemon93 from "../assets/pokemon/front/93.png";
+import pokemon94 from "../assets/pokemon/front/94.png";
+import pokemon95 from "../assets/pokemon/front/95.png";
+import pokemon96 from "../assets/pokemon/front/96.png";
+import pokemon97 from "../assets/pokemon/front/97.png";
+import pokemon98 from "../assets/pokemon/front/98.png";
+import pokemon99 from "../assets/pokemon/front/99.png";
+import pokemon100 from "../assets/pokemon/front/100.png";
+import pokemon101 from "../assets/pokemon/front/101.png";
+import pokemon102 from "../assets/pokemon/front/102.png";
+import pokemon103 from "../assets/pokemon/front/103.png";
+import pokemon104 from "../assets/pokemon/front/104.png";
+import pokemon105 from "../assets/pokemon/front/105.png";
+import pokemon106 from "../assets/pokemon/front/106.png";
+import pokemon107 from "../assets/pokemon/front/107.png";
+import pokemon108 from "../assets/pokemon/front/108.png";
+import pokemon109 from "../assets/pokemon/front/109.png";
+import pokemon110 from "../assets/pokemon/front/110.png";
+import pokemon111 from "../assets/pokemon/front/111.png";
+import pokemon112 from "../assets/pokemon/front/112.png";
+import pokemon113 from "../assets/pokemon/front/113.png";
+import pokemon114 from "../assets/pokemon/front/114.png";
+import pokemon115 from "../assets/pokemon/front/115.png";
+import pokemon116 from "../assets/pokemon/front/116.png";
+import pokemon117 from "../assets/pokemon/front/117.png";
+import pokemon118 from "../assets/pokemon/front/118.png";
+import pokemon119 from "../assets/pokemon/front/119.png";
+import pokemon120 from "../assets/pokemon/front/120.png";
+import pokemon121 from "../assets/pokemon/front/121.png";
+import pokemon122 from "../assets/pokemon/front/122.png";
+import pokemon123 from "../assets/pokemon/front/123.png";
+import pokemon124 from "../assets/pokemon/front/124.png";
+import pokemon125 from "../assets/pokemon/front/125.png";
+import pokemon126 from "../assets/pokemon/front/126.png";
+import pokemon127 from "../assets/pokemon/front/127.png";
+import pokemon128 from "../assets/pokemon/front/128.png";
+import pokemon129 from "../assets/pokemon/front/129.png";
+import pokemon130 from "../assets/pokemon/front/130.png";
+import pokemon131 from "../assets/pokemon/front/131.png";
+import pokemon132 from "../assets/pokemon/front/132.png";
+import pokemon133 from "../assets/pokemon/front/133.png";
+import pokemon134 from "../assets/pokemon/front/134.png";
+import pokemon135 from "../assets/pokemon/front/135.png";
+import pokemon136 from "../assets/pokemon/front/136.png";
+import pokemon137 from "../assets/pokemon/front/137.png";
+import pokemon138 from "../assets/pokemon/front/138.png";
+import pokemon139 from "../assets/pokemon/front/139.png";
+import pokemon140 from "../assets/pokemon/front/140.png";
+import pokemon141 from "../assets/pokemon/front/141.png";
+import pokemon142 from "../assets/pokemon/front/142.png";
+import pokemon143 from "../assets/pokemon/front/143.png";
+import pokemon144 from "../assets/pokemon/front/144.png";
+import pokemon145 from "../assets/pokemon/front/145.png";
+import pokemon146 from "../assets/pokemon/front/146.png";
+import pokemon147 from "../assets/pokemon/front/147.png";
+import pokemon148 from "../assets/pokemon/front/148.png";
+import pokemon149 from "../assets/pokemon/front/149.png";
+import pokemon150 from "../assets/pokemon/front/150.png";
+import pokemon151 from "../assets/pokemon/front/151.png";
+import pokemon152 from "../assets/pokemon/front/152.png";
+import pokemon153 from "../assets/pokemon/front/153.png";
+import pokemon154 from "../assets/pokemon/front/154.png";
+
+export interface PokemonType {
+  image: string;
+}
+
+const pokemonData: Record<number, PokemonType> = {
+  1: { image: pokemon1 },
+  2: { image: pokemon2 },
+  3: { image: pokemon3 },
+  4: { image: pokemon4 },
+  5: { image: pokemon5 },
+  6: { image: pokemon6 },
+  7: { image: pokemon7 },
+  8: { image: pokemon8 },
+  9: { image: pokemon9 },
+  10: { image: pokemon10 },
+  11: { image: pokemon11 },
+  12: { image: pokemon12 },
+  13: { image: pokemon13 },
+  14: { image: pokemon14 },
+  15: { image: pokemon15 },
+  16: { image: pokemon16 },
+  17: { image: pokemon17 },
+  18: { image: pokemon18 },
+  19: { image: pokemon19 },
+  20: { image: pokemon20 },
+  21: { image: pokemon21 },
+  22: { image: pokemon22 },
+  23: { image: pokemon23 },
+  24: { image: pokemon24 },
+  25: { image: pokemon25 },
+  26: { image: pokemon26 },
+  27: { image: pokemon27 },
+  28: { image: pokemon28 },
+  29: { image: pokemon29 },
+  30: { image: pokemon30 },
+  31: { image: pokemon31 },
+  32: { image: pokemon32 },
+  33: { image: pokemon33 },
+  34: { image: pokemon34 },
+  35: { image: pokemon35 },
+  36: { image: pokemon36 },
+  37: { image: pokemon37 },
+  38: { image: pokemon38 },
+  39: { image: pokemon39 },
+  40: { image: pokemon40 },
+  41: { image: pokemon41 },
+  42: { image: pokemon42 },
+  43: { image: pokemon43 },
+  44: { image: pokemon44 },
+  45: { image: pokemon45 },
+  46: { image: pokemon46 },
+  47: { image: pokemon47 },
+  48: { image: pokemon48 },
+  49: { image: pokemon49 },
+  50: { image: pokemon50 },
+  51: { image: pokemon51 },
+  52: { image: pokemon52 },
+  53: { image: pokemon53 },
+  54: { image: pokemon54 },
+  55: { image: pokemon55 },
+  56: { image: pokemon56 },
+  57: { image: pokemon57 },
+  58: { image: pokemon58 },
+  59: { image: pokemon59 },
+  60: { image: pokemon60 },
+  61: { image: pokemon61 },
+  62: { image: pokemon62 },
+  63: { image: pokemon63 },
+  64: { image: pokemon64 },
+  65: { image: pokemon65 },
+  66: { image: pokemon66 },
+  67: { image: pokemon67 },
+  68: { image: pokemon68 },
+  69: { image: pokemon69 },
+  70: { image: pokemon70 },
+  71: { image: pokemon71 },
+  72: { image: pokemon72 },
+  73: { image: pokemon73 },
+  74: { image: pokemon74 },
+  75: { image: pokemon75 },
+  76: { image: pokemon76 },
+  77: { image: pokemon77 },
+  78: { image: pokemon78 },
+  79: { image: pokemon79 },
+  80: { image: pokemon80 },
+  81: { image: pokemon81 },
+  82: { image: pokemon82 },
+  83: { image: pokemon83 },
+  84: { image: pokemon84 },
+  85: { image: pokemon85 },
+  86: { image: pokemon86 },
+  87: { image: pokemon87 },
+  88: { image: pokemon88 },
+  89: { image: pokemon89 },
+  90: { image: pokemon90 },
+  91: { image: pokemon91 },
+  92: { image: pokemon92 },
+  93: { image: pokemon93 },
+  94: { image: pokemon94 },
+  95: { image: pokemon95 },
+  96: { image: pokemon96 },
+  97: { image: pokemon97 },
+  98: { image: pokemon98 },
+  99: { image: pokemon99 },
+  100: { image: pokemon100 },
+  101: { image: pokemon101 },
+  102: { image: pokemon102 },
+  103: { image: pokemon103 },
+  104: { image: pokemon104 },
+  105: { image: pokemon105 },
+  106: { image: pokemon106 },
+  107: { image: pokemon107 },
+  108: { image: pokemon108 },
+  109: { image: pokemon109 },
+  110: { image: pokemon110 },
+  111: { image: pokemon111 },
+  112: { image: pokemon112 },
+  113: { image: pokemon113 },
+  114: { image: pokemon114 },
+  115: { image: pokemon115 },
+  116: { image: pokemon116 },
+  117: { image: pokemon117 },
+  118: { image: pokemon118 },
+  119: { image: pokemon119 },
+  120: { image: pokemon120 },
+  121: { image: pokemon121 },
+  122: { image: pokemon122 },
+  123: { image: pokemon123 },
+  124: { image: pokemon124 },
+  125: { image: pokemon125 },
+  126: { image: pokemon126 },
+  127: { image: pokemon127 },
+  128: { image: pokemon128 },
+  129: { image: pokemon129 },
+  130: { image: pokemon130 },
+  131: { image: pokemon131 },
+  132: { image: pokemon132 },
+  133: { image: pokemon133 },
+  134: { image: pokemon134 },
+  135: { image: pokemon135 },
+  136: { image: pokemon136 },
+  137: { image: pokemon137 },
+  138: { image: pokemon138 },
+  139: { image: pokemon139 },
+  140: { image: pokemon140 },
+  141: { image: pokemon141 },
+  142: { image: pokemon142 },
+  143: { image: pokemon143 },
+  144: { image: pokemon144 },
+  145: { image: pokemon145 },
+  146: { image: pokemon146 },
+  147: { image: pokemon147 },
+  148: { image: pokemon148 },
+  149: { image: pokemon149 },
+  150: { image: pokemon150 },
+  151: { image: pokemon151 },
+  152: { image: pokemon152 },
+  153: { image: pokemon153 },
+  154: { image: pokemon154 },
+};
+
+export default pokemonData;

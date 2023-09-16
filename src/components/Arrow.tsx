9b959a5243d31a346fa0eef6ef28b1840ef050bd
Chase Manning <chase@manning.dev>
2023-09-16 20:56:49 +0100

    :sparkles: add arrow component

---
+++ src/components/Arrow.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+import arrow from "../assets/ui/arrow.png";
+import PixelImage from "../styles/PixelImage";
+
+const StyledArrow = styled(PixelImage)`
+  height: 10px;
+
+  @media (max-width: 768px) {
+    height: 8px;
+  }
+`;
+
+interface Props {
+  show: boolean;
+}
+
+const Arrow = ({ show }: Props) => {
+  if (!show) return null;
+
+  return <StyledArrow src={arrow} />;
+};
+
+export default Arrow;

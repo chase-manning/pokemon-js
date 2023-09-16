4ac760df09d0b8f86b95f85009b3ec66b327e81a
Chase Manning <chase@manning.dev>
2023-09-16 14:03:38 +0100

    :bug: opening music not playing

--- src/components/SoundHandler.tsx
+++ src/components/SoundHandler.tsx
@@ -1,22 +1,33 @@
 import { useSelector } from "react-redux";
 import { selectMap } from "../state/gameSlice";
-import { selectLoadMenu, selectTitleMenu } from "../state/uiSlice";
+import { selectGameboyMenu, selectLoadMenu } from "../state/uiSlice";
 
 import openingMusic from "../assets/music/ui/opening.mp3";
+import mapData from "../maps/map-data";
+import { MapType } from "../maps/map-types";
 
 const SoundHandler = () => {
   const map = useSelector(selectMap);
-  const isTitleScreen = useSelector(selectTitleMenu);
   const isLoadScreen = useSelector(selectLoadMenu);
+  const isGameboyMenu = useSelector(selectGameboyMenu);
 
-  const isOpening = isTitleScreen || isLoadScreen;
+  const meow: string = openingMusic;
+
+  const isOpening = !isGameboyMenu && isLoadScreen;
+
+  const getMapMusic = (map: MapType): string => {
+    if (map.music) return map.music;
+    if (!map.exitReturnMap) throw new Error("map missing music");
+    const returnMap = mapData[map.exitReturnMap];
+    if (!returnMap) throw new Error("return map missing music");
+    return getMapMusic(returnMap);
+  };
 
   const music = () => {
-    if (isOpening) {
-      return openingMusic;
-    }
-
-    return map.music;
+    if (isOpening) return meow;
+    const mapMusic = getMapMusic(map);
+    if (mapMusic) return mapMusic;
+    return undefined;
   };
 
   return <audio autoPlay loop src={music()} />;

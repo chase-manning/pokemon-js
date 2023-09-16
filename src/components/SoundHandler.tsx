1e61a3210664e08e31305688446100228e4a48d6
Chase Manning <chase@manning.dev>
2023-09-16 12:58:37 +0100

    :bug: opening music not playing during load

--- src/components/SoundHandler.tsx
+++ src/components/SoundHandler.tsx
@@ -1,15 +1,18 @@
 import { useSelector } from "react-redux";
 import { selectMap } from "../state/gameSlice";
-import { selectTitleMenu } from "../state/uiSlice";
+import { selectLoadMenu, selectTitleMenu } from "../state/uiSlice";
 
 import openingMusic from "../assets/music/ui/opening.mp3";
 
 const SoundHandler = () => {
   const map = useSelector(selectMap);
   const isTitleScreen = useSelector(selectTitleMenu);
+  const isLoadScreen = useSelector(selectLoadMenu);
+
+  const isOpening = isTitleScreen || isLoadScreen;
 
   const music = () => {
-    if (isTitleScreen) {
+    if (isOpening) {
       return openingMusic;
     }
 

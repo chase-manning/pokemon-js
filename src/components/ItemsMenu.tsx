902fe5999114052e04785da23e8635cc14fd43fc
Chase Manning <chase@manning.dev>
2023-09-16 15:57:05 +0100

    :rocket: implement using and tossing items

--- src/components/ItemsMenu.tsx
+++ src/components/ItemsMenu.tsx
@@ -1,25 +1,68 @@
 import { useDispatch, useSelector } from "react-redux";
 import Menu from "./Menu";
-import { hideItemsMenu, selectItemsMenu } from "../state/uiSlice";
-import { InventoryItemType, selectInventory } from "../state/gameSlice";
+import { hideItemsMenu, selectItemsMenu, showText } from "../state/uiSlice";
+import {
+  InventoryItemType,
+  removeInventory,
+  selectInventory,
+  selectName,
+} from "../state/gameSlice";
+import { useState } from "react";
+import ConfirmationMenu from "./ConfirmationMenu";
 
 const ItemsMenu = () => {
   const dispatch = useDispatch();
   const show = useSelector(selectItemsMenu);
   const inventory = useSelector(selectInventory);
+  const name = useSelector(selectName);
+
+  const [selected, setSelected] = useState<number | null>(null);
+  const [tossing, setTossing] = useState(false);
 
   return (
-    <Menu
-      show={show}
-      close={() => dispatch(hideItemsMenu())}
-      menuItems={inventory.map((item: InventoryItemType) => {
-        return {
-          label: item.item,
-          value: item.amount,
-          action: () => console.log(item.item),
-        };
-      })}
-    />
+    <>
+      <Menu
+        disabled={selected !== null}
+        show={show}
+        close={() => dispatch(hideItemsMenu())}
+        menuItems={inventory
+          .filter((item: InventoryItemType) => item.amount > 0)
+          .map((item: InventoryItemType, index) => {
+            return {
+              label: item.item,
+              value: item.amount,
+              action: () => setSelected(index),
+            };
+          })}
+      />
+      <Menu
+        disabled={tossing}
+        show={selected !== null}
+        close={() => setSelected(null)}
+        menuItems={[
+          {
+            label: "Use",
+            action: () =>
+              dispatch(
+                showText([`OAK: ${name}! This isn't the`, "time to use that!"])
+              ),
+          },
+          {
+            label: "Toss",
+            action: () => setTossing(true),
+          },
+        ]}
+      />
+      {selected !== null && (
+        <ConfirmationMenu
+          show={tossing}
+          preMessage={`Is it OK to toss ${inventory[selected].item}`}
+          postMessage={`${name} tossed ${inventory[selected].item}`}
+          confirm={() => dispatch(removeInventory(inventory[selected]))}
+          cancel={() => setTossing(false)}
+        />
+      )}
+    </>
   );
 };
 

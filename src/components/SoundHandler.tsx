f25008a47b8ff58c76a31c89ef5714e90bee8e17
Chase Manning <chase@manning.dev>
2023-09-17 16:20:38 +0100

    :rocket: add battle sound effects

--- src/components/SoundHandler.tsx
+++ src/components/SoundHandler.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { selectMap } from "../state/gameSlice";
+import { selectMap, selectPokemonEncounter } from "../state/gameSlice";
 import { selectGameboyMenu, selectLoadMenu } from "../state/uiSlice";
 
 import mapData from "../maps/map-data";
@@ -11,6 +11,7 @@ import useEvent from "../app/use-event";
 import openingMusic from "../assets/music/ui/opening.mp3";
 import buttonPress from "../assets/music/ui/button-press.wav";
 import enterDoor from "../assets/music/ui/enter-door.mp3";
+import battleMusic from "../assets/music/ui/battle.mp3";
 
 const SoundHandler = () => {
   const map = useSelector(selectMap);
@@ -18,6 +19,7 @@ const SoundHandler = () => {
   const isGameboyMenu = useSelector(selectGameboyMenu);
   const [uiSound, setUiSound] = useState<string | undefined>(undefined);
   const uiSoundRef = useRef<HTMLAudioElement>(null);
+  const inBattle = !!useSelector(selectPokemonEncounter);
 
   const meow: string = openingMusic;
 
@@ -33,6 +35,7 @@ const SoundHandler = () => {
 
   const music = () => {
     if (isOpening) return meow;
+    if (inBattle) return battleMusic;
     const mapMusic = getMapMusic(map);
     if (mapMusic) return mapMusic;
     return undefined;

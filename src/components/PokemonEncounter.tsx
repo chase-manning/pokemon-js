2f7a1105b122d228859c948a5e088d7f606fa581
Chase Manning <chase@manning.dev>
2023-09-17 16:20:32 +0100

    :rocket: add battle intro animation

--- src/components/PokemonEncounter.tsx
+++ src/components/PokemonEncounter.tsx
@@ -1,6 +1,7 @@
-import { useSelector } from "react-redux";
-import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import styled, { keyframes } from "styled-components";
 import {
+  endEncounter,
   selectActivePokemon,
   selectPokemonEncounter,
 } from "../state/gameSlice";
@@ -10,6 +11,9 @@ import HealthBar from "./HealthBar";
 import usePokemonStats from "../app/use-pokemon-stats";
 
 import corner from "../assets/ui/corner.png";
+import { useEffect, useState } from "react";
+import useEvent from "../app/use-event";
+import { Event } from "../app/emitter";
 
 const StyledPokemonEncounter = styled.div`
   position: absolute;
@@ -134,7 +138,135 @@ const TextContainer = styled.div`
   }
 `;
 
+const moveLeft = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+`;
+
+const moveRight = keyframes`
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+`;
+
+const RightSide = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0) 5%,
+    rgba(0, 0, 0, 1) 5%,
+    rgba(0, 0, 0, 1) 10%,
+    rgba(0, 0, 0, 0) 10%,
+    rgba(0, 0, 0, 0) 15%,
+    rgba(0, 0, 0, 1) 15%,
+    rgba(0, 0, 0, 1) 20%,
+    rgba(0, 0, 0, 0) 20%,
+    rgba(0, 0, 0, 0) 25%,
+    rgba(0, 0, 0, 1) 25%,
+    rgba(0, 0, 0, 1) 30%,
+    rgba(0, 0, 0, 0) 30%,
+    rgba(0, 0, 0, 0) 35%,
+    rgba(0, 0, 0, 1) 35%,
+    rgba(0, 0, 0, 1) 40%,
+    rgba(0, 0, 0, 0) 40%,
+    rgba(0, 0, 0, 0) 45%,
+    rgba(0, 0, 0, 1) 45%,
+    rgba(0, 0, 0, 1) 50%,
+    rgba(0, 0, 0, 0) 50%,
+    rgba(0, 0, 0, 0) 55%,
+    rgba(0, 0, 0, 1) 55%,
+    rgba(0, 0, 0, 1) 60%,
+    rgba(0, 0, 0, 0) 60%,
+    rgba(0, 0, 0, 0) 65%,
+    rgba(0, 0, 0, 1) 65%,
+    rgba(0, 0, 0, 1) 70%,
+    rgba(0, 0, 0, 0) 70%,
+    rgba(0, 0, 0, 0) 75%,
+    rgba(0, 0, 0, 1) 75%,
+    rgba(0, 0, 0, 1) 80%,
+    rgba(0, 0, 0, 0) 80%,
+    rgba(0, 0, 0, 0) 85%,
+    rgba(0, 0, 0, 1) 85%,
+    rgba(0, 0, 0, 1) 90%,
+    rgba(0, 0, 0, 0) 90%,
+    rgba(0, 0, 0, 0) 95%,
+    rgba(0, 0, 0, 1) 95%,
+    rgba(0, 0, 0, 1) 100%
+  );
+
+  transform: translateX(100%);
+
+  animation: ${moveLeft} 1500ms linear forwards;
+`;
+
+const LevtSide = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(0, 0, 0, 1) 5%,
+    rgba(0, 0, 0, 0) 5%,
+    rgba(0, 0, 0, 0) 10%,
+    rgba(0, 0, 0, 1) 10%,
+    rgba(0, 0, 0, 1) 15%,
+    rgba(0, 0, 0, 0) 15%,
+    rgba(0, 0, 0, 0) 20%,
+    rgba(0, 0, 0, 1) 20%,
+    rgba(0, 0, 0, 1) 25%,
+    rgba(0, 0, 0, 0) 25%,
+    rgba(0, 0, 0, 0) 30%,
+    rgba(0, 0, 0, 1) 30%,
+    rgba(0, 0, 0, 1) 35%,
+    rgba(0, 0, 0, 0) 35%,
+    rgba(0, 0, 0, 0) 40%,
+    rgba(0, 0, 0, 1) 40%,
+    rgba(0, 0, 0, 1) 45%,
+    rgba(0, 0, 0, 0) 45%,
+    rgba(0, 0, 0, 0) 50%,
+    rgba(0, 0, 0, 1) 50%,
+    rgba(0, 0, 0, 1) 55%,
+    rgba(0, 0, 0, 0) 55%,
+    rgba(0, 0, 0, 0) 60%,
+    rgba(0, 0, 0, 1) 60%,
+    rgba(0, 0, 0, 1) 65%,
+    rgba(0, 0, 0, 0) 65%,
+    rgba(0, 0, 0, 0) 70%,
+    rgba(0, 0, 0, 1) 70%,
+    rgba(0, 0, 0, 1) 75%,
+    rgba(0, 0, 0, 0) 75%,
+    rgba(0, 0, 0, 0) 80%,
+    rgba(0, 0, 0, 1) 80%,
+    rgba(0, 0, 0, 1) 85%,
+    rgba(0, 0, 0, 0) 85%,
+    rgba(0, 0, 0, 0) 90%,
+    rgba(0, 0, 0, 1) 90%,
+    rgba(0, 0, 0, 1) 95%,
+    rgba(0, 0, 0, 0) 95%,
+    rgba(0, 0, 0, 0) 100%
+  );
+
+  transform: translateX(-100%);
+  animation: ${moveRight} 1500ms linear forwards;
+`;
+
 const PokemonEncounter = () => {
+  const dispatch = useDispatch();
   const enemy = useSelector(selectPokemonEncounter);
   const enemyMetadata = usePokemonMetadata(enemy?.id || null);
   const enemyStats = usePokemonStats(enemy?.id || 1, enemy?.level || 1);
@@ -142,54 +274,89 @@ const PokemonEncounter = () => {
   const activeMetadata = usePokemonMetadata(active?.id || null);
   const activeStats = usePokemonStats(active?.id || 1, active?.level || 1);
 
-  if (!enemy || !enemyMetadata || !activeMetadata || !active) return null;
+  const [animationDone, setAnimationDone] = useState(false);
+  const [animationStarted, setAnimationStarted] = useState(false);
+
+  const isInBattle = !!enemy && !!active && !!enemyMetadata && !!activeMetadata;
+
+  useEffect(() => {
+    if (isInBattle) {
+      setAnimationStarted(true);
+      setTimeout(() => {
+        setAnimationDone(true);
+      }, 2000);
+    }
+
+    if (!isInBattle) {
+      setAnimationDone(false);
+      setAnimationStarted(false);
+    }
+  }, [isInBattle]);
+
+  // TEMP
+  useEvent(Event.B, () => {
+    if (!animationDone) return;
+    dispatch(endEncounter());
+  });
+
+  if (!isInBattle) return null;
 
   return (
     <>
-      <StyledPokemonEncounter>
-        <Row>
-          <LeftInfoSection>
-            <Name>{enemyMetadata.name}</Name>
-            <Level>{`:L${enemy.level}`}</Level>
-            <HealthBarContainer>
-              <HealthBar
-                big
-                currentHealth={enemy.hp}
-                maxHealth={enemyStats.hp}
-              />
-            </HealthBarContainer>
-            <Corner src={corner} />
-          </LeftInfoSection>
-          <ImageContainer>
-            <Image src={enemyMetadata.images.front} />
-          </ImageContainer>
-        </Row>
-        <Row>
-          <ImageContainer>
-            <Image src={activeMetadata.images.back} />
-          </ImageContainer>
-          <RightInfoSection>
-            <Name>{activeMetadata.name}</Name>
-            <Level>{`:L${active.level}`}</Level>
-            <HealthBarContainer>
-              <HealthBar
-                big
-                currentHealth={active.hp}
-                maxHealth={activeStats.hp}
-              />
-            </HealthBarContainer>
-            <Health>{`${active.hp}/${activeStats.hp}`}</Health>
-            <CornerContainer>
-              <CornerRight src={corner} />
-            </CornerContainer>
-          </RightInfoSection>
-        </Row>
-      </StyledPokemonEncounter>
-      <TextContainer>
-        <Frame wide tall>
-          meow
-        </Frame>
-      </TextContainer>
+      {animationStarted && !animationDone && (
+        <>
+          <RightSide />
+          <LevtSide />
+        </>
+      )}
+      {animationDone && (
+        <>
+          <StyledPokemonEncounter>
+            <Row>
+              <LeftInfoSection>
+                <Name>{enemyMetadata.name}</Name>
+                <Level>{`:L${enemy.level}`}</Level>
+                <HealthBarContainer>
+                  <HealthBar
+                    big
+                    currentHealth={enemy.hp}
+                    maxHealth={enemyStats.hp}
+                  />
+                </HealthBarContainer>
+                <Corner src={corner} />
+              </LeftInfoSection>
+              <ImageContainer>
+                <Image src={enemyMetadata.images.front} />
+              </ImageContainer>
+            </Row>
+            <Row>
+              <ImageContainer>
+                <Image src={activeMetadata.images.back} />
+              </ImageContainer>
+              <RightInfoSection>
+                <Name>{activeMetadata.name}</Name>
+                <Level>{`:L${active.level}`}</Level>
+                <HealthBarContainer>
+                  <HealthBar
+                    big
+                    currentHealth={active.hp}
+                    maxHealth={activeStats.hp}
+                  />
+                </HealthBarContainer>
+                <Health>{`${active.hp}/${activeStats.hp}`}</Health>
+                <CornerContainer>
+                  <CornerRight src={corner} />
+                </CornerContainer>
+              </RightInfoSection>
+            </Row>
+          </StyledPokemonEncounter>
+          <TextContainer>
+            <Frame wide tall>
+              meow
+            </Frame>
+          </TextContainer>
+        </>
+      )}
     </>
   );
 };

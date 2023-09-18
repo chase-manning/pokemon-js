191fc0ef60f4c9585260709e308ae9caf70dda7e
Chase Manning <chase@manning.dev>
2023-09-18 23:42:24 +0100

    :sparkles: add critical constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,5 @@
 export const BLOCK_PIXEL_WIDTH = 16;
 export const BLOCK_PIXEL_HEIGHT = 16;
 export const WALK_SPEED = 150;
+export const CRITICAL_HIT_PERCENTAGE = 0.1;
+export const CRITICAL_HIT_MULTIPLIER = 2;

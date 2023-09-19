34887866aade4ba2291a6c26d0449dae9bb0a760
Chase Manning <chase@manning.dev>
2023-09-19 09:58:06 +0100

    :bug: non-damage attacks throwing error

--- src/app/move-helper.ts
+++ src/app/move-helper.ts
@@ -6,11 +6,14 @@ import { getPokemonMetadata } from "./use-pokemon-metadata";
 import { getPokemonStats } from "./use-pokemon-stats";
 
 export interface MoveResult {
+  moveName: string;
   us: PokemonInstance;
   them: PokemonEncounterType;
   missed: boolean;
   superEffective: boolean;
   critical: boolean;
+  isBuff: boolean;
+  isDebuff: boolean;
 }
 
 const processMove = (
@@ -25,19 +28,32 @@ const processMove = (
   const theirStats = getPokemonStats(them.id, them.level);
   const moveMetadata = getMoveMetadata(move);
 
+  const defaultReturn = {
+    moveName: moveMetadata.name,
+    us,
+    them,
+    missed: true,
+    superEffective: false,
+    critical: false,
+    isBuff: false,
+    isDebuff: false,
+  };
+
   // Attack misses
   if (moveMetadata.accuracy && moveMetadata.accuracy < Math.random() * 100) {
-    return {
-      us,
-      them,
-      missed: true,
-      superEffective: false,
-      critical: false,
-    };
+    return defaultReturn;
   }
 
   // Our attack
   if (isAttacking) {
+    // TODO
+    if (!moveMetadata.power) {
+      return {
+        ...defaultReturn,
+        isDebuff: true,
+      };
+    }
+
     const attack =
       moveMetadata.damageClass === "physical"
         ? ourStats.attack
@@ -67,18 +83,25 @@ const processMove = (
     );
 
     return {
-      us,
+      ...defaultReturn,
       them: {
         ...them,
         hp: Math.max(0, them.hp - damage),
       },
-      missed: false,
       superEffective,
       critical: critical > 1,
     };
   }
 
   // Enemy attack
+  // TODO
+  if (!moveMetadata.power) {
+    return {
+      ...defaultReturn,
+      isDebuff: true,
+    };
+  }
+
   const attack =
     moveMetadata.damageClass === "physical"
       ? theirStats.attack
@@ -107,12 +130,11 @@ const processMove = (
   );
 
   return {
+    ...defaultReturn,
     us: {
       ...us,
       hp: Math.max(0, us.hp - damage),
     },
-    them,
-    missed: false,
     superEffective,
     critical: critical > 1,
   };

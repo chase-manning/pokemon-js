e7b45cf76aebdcef3b3a8feb07d8f45aa384740a
Chase Manning <chase@manning.dev>
2023-09-20 13:34:08 +0100

    :bug: xp not rounded

--- src/app/xp-helper.ts
+++ src/app/xp-helper.ts
@@ -2,7 +2,7 @@ import { getPokemonMetadata } from "./use-pokemon-metadata";
 
 const getXp = (enemyId: number, enemyLevel: number) => {
   const metadata = getPokemonMetadata(enemyId);
-  return (metadata.baseExperience * enemyLevel) / 6;
+  return Math.round((metadata.baseExperience * enemyLevel) / 6);
 };
 
 export default getXp;

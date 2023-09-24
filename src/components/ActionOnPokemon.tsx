ff2d60c2619713fb66c324dcce3faee200a62b9e
Chase Manning <chase@manning.dev>
2023-09-24 11:37:56 +0100

    :sparkles: create action on pokemon component

---
+++ src/components/ActionOnPokemon.tsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { hideActionOnPokemon, selectActionOnPokemon } from "../state/uiSlice";
+import PokemonList from "./PokemonList";
+
+const ActionOnPokemon = () => {
+  const dispatch = useDispatch();
+  const action = useSelector(selectActionOnPokemon);
+
+  if (!action) return null;
+
+  return (
+    <PokemonList
+      close={() => dispatch(hideActionOnPokemon())}
+      clickPokemon={(index) => action(index)}
+      text="Choose a Pokémon."
+    />
+  );
+};
+
+export default ActionOnPokemon;

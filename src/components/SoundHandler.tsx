6905e9838228d9e289c36108383b7d9978363d88
Chase Manning <chase@manning.dev>
2023-09-25 17:20:51 +0100

    :rocket: add pokemon center sound effects

--- src/components/SoundHandler.tsx
+++ src/components/SoundHandler.tsx
@@ -12,6 +12,7 @@ import openingMusic from "../assets/music/ui/opening.mp3";
 import buttonPress from "../assets/music/ui/button-press.wav";
 import enterDoor from "../assets/music/ui/enter-door.mp3";
 import battleMusic from "../assets/music/ui/battle.mp3";
+import healPokemon from "../assets/music/ui/pokemon-recovery.mp3";
 
 const SoundHandler = () => {
   const map = useSelector(selectMap);
@@ -20,6 +21,8 @@ const SoundHandler = () => {
   const [uiSound, setUiSound] = useState<string | undefined>(undefined);
   const uiSoundRef = useRef<HTMLAudioElement>(null);
   const inBattle = !!useSelector(selectPokemonEncounter);
+
+  const [musicOverride, setMusicOverride] = useState(null);
 
   const meow: string = openingMusic;
 
@@ -36,6 +39,7 @@ const SoundHandler = () => {
   const music = () => {
     if (isOpening) return meow;
     if (inBattle) return battleMusic;
+    if (musicOverride) return musicOverride;
     const mapMusic = getMapMusic(map);
     if (mapMusic) return mapMusic;
     return undefined;
@@ -56,6 +60,13 @@ const SoundHandler = () => {
 
   useEvent(Event.EnterDoor, () => {
     playUiSound(enterDoor);
+  });
+
+  useEvent(Event.HealPokemon, () => {
+    setMusicOverride(healPokemon);
+    setTimeout(() => {
+      setMusicOverride(null);
+    }, 3000);
   });
 
   return (
